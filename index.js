632e06735a1afe32244b2c188b38c9e64750db50
destanyb <52415719+destanyb@users.noreply.github.com>
2019-07-21 21:32:22 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-$("#form1").submit(function (e) {
+$("#form_id").submit(function (e) {
     e.preventDefault();
+    var input_value = document.getElementById("link_input").value;
+    var image = document.createElement("IMG");
+    image.src = input_value;
+  
+    var box = document.createElement("div")
+    var caption = document.createElement("p")
+    var caption_value = document.getElementById("caption").value
+    caption.innerHTML = caption_value
 
-    var todo_input = document.getElementById("todo-input");
-    var todo_text = todo_input.value;
+    box.appendChild(image)
+    box.appendChild(caption)
+    document.getElementById("container").appendChild(box);
 
-    addNewItem(todo_text);
+       box.style.height = 150 + "px";
+       box.style.padding = 10 + "px"
+       box.style.backgroundColor = "white"; 
+       box.style.width = 150 + "px";
+       box.style.overflowY = "auto";
+       box.style.overflowX = "auto";
+       box.style.wordWrap = "break-word";
+       box.style.display = "inline-block";
 
-    todo_input.value = "";
+       image.style.width = 149 + "px";
+
+
+      box.addEventListener("click", function () { //event listener looks for a click of the post and then closes it
+        var box_id = String(Math.random()); 
+        box.id = box_id; 
+
+        document.getElementById(box_id).remove(); 
+    });   
+	
+	
+	
+	
 });
-
-
-function addNewItem(todo_text) {
-    var todo_card = document.createElement("div");
-    todo_card.classList.add("todo_card");
-
-    var todo_card_id = String(Math.random())
-    todo_card.id = todo_card_id
-
-    var todo_text_elem = document.createElement("p");
-    todo_text_elem.innerHTML = todo_text;
-
-    todo_card.appendChild(todo_text_elem);
-
-    document.getElementById("container").appendChild(todo_card);
-    todo_card.addEventListener("click", function () {
-        document.getElementById(todo_card_id).remove()
-    });
-
-}
